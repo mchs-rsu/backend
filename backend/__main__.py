5354d97ad120bd019dac3402c25c47fa5b27ce13
Art Gilyazov <art33art@yandex.ru>
2022-12-02 06:31:15 +0300

    add views for dictricts

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,12 +1,35 @@
 import logging
 
+from flask import Flask
+from pydantic import ValidationError
+
+from backend.database import db_session
+from backend.disticts.views import district_view
+
+
+app = Flask(__name__)
+app.register_blueprint(district_view, url_prefix='/api/districts')
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+def handle_validation_error(err: ValidationError):
+    return {'error': str(err)}, 400
+
+
+app.register_error_handler(ValidationError, handle_validation_error)
+app.teardown_appcontext(shutdown_session)
+
+
 def main():
-    logger('Started successfully')
+    logger.info('Started successfully')
+    app.run(host='127.0.0.1', port=5000)
 
 
 if __name__ == '__main__':
---
+++ backend/disticts/views.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, request
+
+from backend.disticts.schemas import District
+from backend.disticts.storage import WebStorage
+
+
+district_view = Blueprint('districts', __name__)
+storage = WebStorage()
+
+
+@district_view.post('/')
+def add():
+    payload = request.json
+
+    if not payload:
+        """здесь будет raise ошибки empty payload"""
+
+        payload['uid'] = -1
+
+        district = District(**payload)
+        district = storage.add(district)
+
+        return district.dict(), 201
+
+
+@district_view.get('/<int:uid>')
+def get_by_id(uid):
+    district = storage.get_by_id(uid)
+
+    return district.dict(), 200
+
+
+@district_view.delete('/<int:uid>')
+def delete_district(uid):
+    storage.delete(uid)
+
+    return {}, 204
+
+
+@district_view.put('/<int:uid>')
+def update_by_id(uid):
+    payload = request.json
+
+    if not payload:
+        """здесь будет raise ошибки empty payload"""
+
+    payload['uid'] = uid
+    district = District(**payload)
+    district = storage.update(uid, district)
+
+    return district.dict(), 200
+
+
+@district_view.get('/')
+def get_all(name=''):
+    if 'name' in request.args:
+        name = request.args.get('name')
+        districts = storage.get_by_name(name)
+    else:
+        districts = storage.get_all()
+
+    return [district.dict() for district in districts], 200
